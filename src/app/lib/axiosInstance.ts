724777f3cb615eeece1beda7070bc07e73e4845e
TaekoHarada <taecooo512@gmail.com>
2025-02-19 15:48:13 -0700

    update

--- src/app/lib/axiosInstance.ts
+++ src/app/lib/axiosInstance.ts
@@ -1,7 +1,6 @@
 import axios from "axios";
 
-const BASE_URL =
-  process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:5000";
+const BASE_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:3000";
 
 const axiosInstance = axios.create({
   baseURL: BASE_URL,
